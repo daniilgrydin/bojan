6e9e6379947add332a20792d4711b9ea4c49e683
Daniil Grydin <61776829+daniilgrydin@users.noreply.github.com>
2024-11-26 19:21:12 -0800

    Update run.py: enhance console output with depth parameter and add logging functionality

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import bojan
 
-cons = bojan.BojanConsole()
-cons.success("Hello, World!")
+console = bojan.BojanConsole()
+console.print("Message!", depth=0)
+console.print("Nested message!", depth=1)
+console.error("Nested error message!", depth=1)
+console.save("log.bjn")
